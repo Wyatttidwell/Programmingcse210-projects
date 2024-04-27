77fb18dd95ad83e215fcd559f3fe4829cc4622c4
Wyatttidwell <Wyatttidwell@users.noreply.github.com>
2024-04-27 15:52:01 -0600

    git commit -m "Name thingy"

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Welcome to the Nameifier");
+
+        Console.Write("What is your First Name? ");
+        
+        string name1 = Console.ReadLine();
+        Console.WriteLine("Thank you");
+        Console.Write("What is your Last Name? ");
+        string name2 = Console.ReadLine();
+        Console.WriteLine("Thank you");
+        Console.WriteLine($"Your name is {name2}, {name1} {name2}");
     }
 }
