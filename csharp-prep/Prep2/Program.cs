9e196929d42d9810b167d464d93111f13cb20693
Wyatttidwell <Wyatttidwell@users.noreply.github.com>
2024-04-27 19:44:46 -0600

    git commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your Grade Percentage?");
+        string userpercentage = Console.ReadLine();
+        int x = int.Parse(userpercentage);
+
+        string gradeletter = "";
+
+        if(x >= 90)
+        gradeletter = "A";
+        else if (x >= 80)
+        gradeletter = "B";
+        else if (x >= 70)
+        gradeletter = "C";
+        else if (x >= 60)
+        gradeletter = "D";
+        else 
+        gradeletter = "F";
+    Console.WriteLine($"Your Grade is {gradeletter}");
+    if (x >= 70)
+    Console.WriteLine("congrats you passed");
+    else
+    Console.WriteLine("Looks like you failed, ByteConverter luck next time");
     }
 }
