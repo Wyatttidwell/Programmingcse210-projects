54ea0810f0e90a71836d1be189d92bc04bb3ef07
Wyatttidwell <Wyatttidwell@users.noreply.github.com>
2024-05-01 21:46:50 -0600

    bit commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
-{
-    static void Main(string[] args)
+
+{static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+
+        int Numberguess = randomGenerator.Next(1, 100);
+
+        int guess = 1;
+
+        while (guess != Numberguess)
+        {
+            Console.Write("Try to guess the number. "); 
+
+            guess = int.Parse(Console.ReadLine());
+            
+            if (Numberguess > guess)
+            {
+                Console.WriteLine("The magic number is higher than your number.");
+            }
+
+
+            else if (Numberguess < guess) 
+            {
+                Console.WriteLine("The magic number is lower than your number.");
+            }
+
+            else 
+            {
+                Console.WriteLine("you guessed it!");
+            }
+        }
+    
+    
     }
 }
