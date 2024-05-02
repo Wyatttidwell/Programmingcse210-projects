e3480a8df41de4a8fabedc74f238073d63a56f3b
Wyatttidwell <Wyatttidwell@users.noreply.github.com>
2024-05-01 23:34:22 -0600

    Lit Commit

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,34 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+    static void Main(string[] args) {
+        DisplayWelcomeMessage();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(name, square);
+    }
+    static void DisplayWelcomeMessage(){
+        Console.WriteLine("Beinvenido to my program!!!");
+        }
+
+    static string PromptUserName(){
+        Console.Write("Your name is? = ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int PromptUserNumber() {
+        Console.Write("please enter a number ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number){
+        int square = number * number;
+        return square;
+    }
+    static void DisplayResult(string name, int square) {
+        Console.WriteLine($"Fun fact {name}, your number but squared is {square}");
     }
 }
